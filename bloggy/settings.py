8b8ffd06bbb534f89f4b1f414087fd522242f78f
Gbenga Sokunbi <dtechydude@gmail.com>
2021-12-28 16:40:04 +0100

    add requirements.txt

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bloggyblogapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
