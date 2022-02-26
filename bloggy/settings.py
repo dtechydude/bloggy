cf96188bcaf1c6c92b8d89cc9b199f694fc56cfa
Gbenga Sokunbi <dtechydude@gmail.com>
2022-02-26 20:00:13 +0100

    environ setting

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 #setting the environment variables----
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=(bool, True)
 )
 environ.Env.read_env()
 #setting the environment variables END----
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
