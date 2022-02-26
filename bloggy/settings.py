3c32e369946d0b5b452247cbdf56e4239821cc4d
Gbenga Sokunbi <dtechydude@gmail.com>
2022-02-26 19:49:32 +0100

    environ setting

--- bloggy/settings.py
+++ bloggy/settings.py
@@ -31,10 +31,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+# SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dtechyblog.herokuapp.com', '127.0.0.1']
 
